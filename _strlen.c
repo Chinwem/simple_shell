64d8b637579931cb9c5b6372b6661a92164fd8b6
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-04-26 18:56:08 -0700

    strlen function

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include <shell.h>
+
+/**
+ * _strlen - Checks string length
+ * @s: Variable to check string length
+ * Return: Always number counted
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
